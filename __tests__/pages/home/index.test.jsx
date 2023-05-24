be0b53e57ab6aeea901af2d1d33d7392f0ba5f32
Zhenhui Wang <zhenhuismac@Zhenhuis-Mac-mini.local>
2023-05-24 13:11:58 +1000

    fix: add prettier and set running prettier and test before commit

--- __tests__/pages/home/index.test.jsx
+++ __tests__/pages/home/index.test.jsx
@@ -1,13 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../../../src/pages/index';
-import '@testing-library/jest-dom';
- 
-describe('Home', () => {
-  it('renders welcome text within home page', () => {
-    render(<Home/>);
+import { render, screen } from "@testing-library/react";
+import Home from "../../../src/pages/index";
+import "@testing-library/jest-dom";
 
-    const welcomeText = screen.getByText('Hello, this is TaoTalk');
- 
+describe("Home", () => {
+  it("renders welcome text within home page", () => {
+    render(<Home />);
+
+    const welcomeText = screen.getByText("Hello, this is TaoTalk");
+
     expect(welcomeText).toBeInTheDocument();
   });
 });
