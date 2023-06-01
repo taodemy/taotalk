e1746bbee0031560a649e505f87e7ec24b8bebac
Pengcheng Yu <ypc624@outlook.com>
2023-06-01 17:16:14 +1000

    fix: put head element in the page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Layout from '../layout';
-import Head from 'next/head';
 
 const TaoTalkApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Head>
-        <title>TaoTalk</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="TaoTalk" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
