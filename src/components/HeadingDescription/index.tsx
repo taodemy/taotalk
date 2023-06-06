91fa1101d85df960ce84c3f65d14304067309cc8
Jason Wang <0212jason@gmail.com>
2023-06-06 13:18:13 +1000

    feat: static static page draft for OtherSections

---
+++ src/components/HeadingDescription/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeadingDescription = () => {
+  return (
+    <div>
+      <h2>Learn a language in a playful way</h2>
+      <p>Make learning words more fun with mini-games</p>
+    </div>
+  );
+};
+
+export default HeadingDescription;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,64 @@
+import Image from 'next/image';
+import LanguageImage from '../../public/language.svg';
+import VocabularyImage from '../../public/vocabulary.svg';
+import ProgressImage from '../../public/progress.svg';
+import HeadingDescription from '../components/HeadingDescription';
+import Button from '../components/Shared/Buttons';
+
 export default function Home() {
   return (
     <>
       <section>
         <h1>Learning and teaching online, made easy.</h1>
       </section>
-      <section>
-        <h2>Learn a language in a playful way</h2>
-      </section>
-      <section>
-        <h2>Increase your vocabulary</h2>
-      </section>
-      <section>
-        <h2>Watch your progress everyday</h2>
+      <section style={{ width: '100%' }}>
+        <section
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            backgroundColor: '#FFFFF',
+            position: 'relative',
+          }}
+        >
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ width: '380px', border: '3px solid red' }}>
+              <Image src={LanguageImage} width={280} alt="Language" />
+            </div>
+            <div style={{ width: '380px', border: '3px solid red' }}>
+              <HeadingDescription />
+            </div>
+          </div>
+          {/* <h2>Learn a language in a playful way</h2> */}
+        </section>
+        <section style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#F6F5F4' }}>
+          {/* <h2>Increase your vocabulary</h2> */}
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ width: '380px', border: '3px solid red' }}>
+              <HeadingDescription />
+              <Button color="light" label="Textbook->" />
+            </div>
+            <div style={{ width: '380px', border: '3px solid red', position: 'relative' }}>
+              <Image
+                src={VocabularyImage}
+                width={280}
+                alt="Vocabulary"
+                style={{ marginLeft: '-50px' }}
+              />
+            </div>
+          </div>
+        </section>
+        <section style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#FFFFF' }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ width: '380px', border: '3px solid red' }}>
+              <Image src={ProgressImage} height={230} alt="Language" />
+            </div>
+            <div style={{ width: '380px', border: '3px solid red' }}>
+              <HeadingDescription />
+              <Button color="light" label="Statistics->" />
+            </div>
+          </div>
+          {/* <h2>Watch your progress everyday</h2> */}
+        </section>
       </section>
     </>
   );
