56b3334756d3f701d89c82f362c50299bb50012f
Jason Wang <0212jason@gmail.com>
2023-06-06 13:18:13 +1000

    feat: static static page draft for OtherSections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../layout';
+import '../../src/normalize.scss';
 
 const TaoTalkApp = ({ Component, pageProps }: AppProps) => {
   return (
