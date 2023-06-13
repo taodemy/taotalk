4f9e439a77bef0dfb212efb9bc8b221fc7c678fa
Jason Wang <0212jason@gmail.com>
2023-06-13 10:41:49 +1000

    feat: sprint game dashboard and summary draft

---
+++ src/components/DashInfo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const PointsDash = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <p>retrieved</p>
+      <h3>240</h3>
+      <p>points</p>
+    </div>
+  );
+};
