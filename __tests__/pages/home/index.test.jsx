be14006558a0695fdfb767b2f27b71d224c136e9
Lu Liu <luliu@Lus-MacBook-Pro.local>
2023-06-14 19:45:20 +1000

    feat: add tests for hero

--- __tests__/pages/home/index.test.jsx
+++ __tests__/pages/home/index.test.jsx
@@ -1,13 +1,72 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../../../src/pages/index';
 import '@testing-library/jest-dom';
+import exp from 'constants';
 
 describe('Home', () => {
-  it('renders welcome text within home page', () => {
+  beforeEach(() => {
     render(<Home />);
+  });
+  // Tests for Hero section
+  it('renders a hero subheading', () => {
+    const subheading = screen.getByText('AI-powered chatting PLATFORM');
+    expect(subheading).toBeInTheDocument();
+  });
 
-    const welcomeText = screen.getByText('Learning and teaching online, made easy.');
+  it('renders a hero heading', () => {
+    const heroHeading = screen.getByText('Learning and teaching online, made easy.');
+    expect(heroHeading).toBeInTheDocument();
+  });
 
-    expect(welcomeText).toBeInTheDocument();
+  it('renders a hero content', () => {
+    const content = screen.getByText(
+      'Practice your English and learn new things with the platform.'
+    );
+    expect(content).toBeInTheDocument();
+  });
+
+  it('renders a left number', () => {
+    const numberLeft = screen.getByText('600');
+    expect(numberLeft).toBeInTheDocument();
+  });
+
+  it('renders a right number', () => {
+    const numberRight = screen.getByText('2');
+    expect(numberRight).toBeInTheDocument();
+  });
+  it('renders two plus letter', () => {
+    const plus = screen.getAllByText('+');
+    expect(plus[0]).toBeInTheDocument();
+    expect(plus[1]).toBeInTheDocument();
+  });
+
+  it('renders two texts', () => {
+    const textLeft = screen.getByText('Popular words');
+    const textRight = screen.getByText('Mini-games');
+    expect(textLeft).toBeInTheDocument();
+    expect(textRight).toBeInTheDocument();
+  });
+
+  it('renders a hero button', () => {
+    const button = screen.getByRole('button', { name: /About platform/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders a lightening icon', () => {
+    const lighteningIcon = screen.getByRole('img', { name: /heroLightening icon/i });
+    expect(lighteningIcon).toBeInTheDocument();
+    expect(lighteningIcon).toHaveAttribute('src', '/heroLightening.svg');
+  });
+
+  it('renders a game icon', () => {
+    const gameIcon = screen.getByRole('img', { name: /heroGame icon/i });
+    expect(gameIcon).toBeInTheDocument();
+    expect(gameIcon).toHaveAttribute('src', '/heroGame.svg');
+  });
+
+  it('renders a boy image', () => {
+    const boyImg = screen.getByRole('img', { name: /heroBoy image/i });
+    expect(boyImg).toBeInTheDocument();
+    expect(boyImg).toHaveAttribute('src', '/heroBoy.svg');
   });
 });
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -9,25 +9,27 @@ const Hero = () => {
           Practice your English and learn new things with the platform.
         </p>
         <Button color="light" label="About Platform" />
+
         <div className="hero__figure">
           <div className="hero__figure_left">
             <h3 className="heading--h3">
-              <img src="/heroLightening.svg" alt="" />
+              <img src="/heroLightening.svg" alt="heroLightening icon" />
               600<span>+</span>
             </h3>
-
             <p className="paragraph--p2">Popular words</p>
           </div>
+
           <div className="divider"></div>
+
           <div className="hero__figure_right">
             <h3 className="heading--h3">
-              <img src="/heroGame.svg" alt="" />2<span>+</span>
+              <img src="/heroGame.svg" alt="heroGame icon" />2<span>+</span>
             </h3>
             <p className="paragraph--p2">Mini-games</p>
           </div>
         </div>
       </div>
-      <img src="/heroBoy.svg" alt="" />
+      <img src="/heroBoy.svg" alt="heroBoy image" />
     </section>
   );
 };
