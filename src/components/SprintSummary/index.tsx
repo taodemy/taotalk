77d2763e086e35442adc66bc8d14e02a51d5cc54
Jason Wang <0212jason@gmail.com>
2023-06-17 21:00:36 +1000

    fix: extract SummaryFrame out to make it can be used for new audio call summary page

--- src/components/SprintSummary/index.tsx
+++ src/components/SprintSummary/index.tsx
@@ -1,97 +1,35 @@
 import React from 'react';
-import Button from '../../components/Shared/Buttons';
-import ScrollArea from '../../components/Shared/ScrollArea';
-import Label from '../../components/Shared/Label';
 import CircularDashboard from '../../components/Shared/CircularDashboard';
 import { PointsDash, WordsDash } from '../../components/DashInfo';
 import styles from './SprintSummary.module.scss';
-
-interface TitleListProps {
-  titleName: string;
-  labelName: string;
-  labelColor: string;
-  labelBgColor: string;
-}
-const TitleList: React.FC<TitleListProps> = ({
-  titleName,
-  labelName,
-  labelColor,
-  labelBgColor,
-}) => {
-  return (
-    <div>
-      <div className={styles.titleListContainer}>
-        <h3 className="heading--h3">{titleName}</h3>
-        <Label fontColor={`${labelColor}`} bgColor={`${labelBgColor}`}>
-          {labelName}
-        </Label>
-      </div>
-      <ScrollArea />
-    </div>
-  );
-};
+import SummaryFrame from '../Shared/SummaryFrame';
 
 const SprintSummary = () => {
   return (
-    <section className={styles.sprintSummaryContainer}>
-      <div>
-        <div className={styles.summaryDetails}>
-          <img src="./books.svg" alt="books" />
-          <div>
-            <h3 className="heading--h3">Your Sprint</h3>
-            <p className={`paragraph--p2 ${styles.summaryComment}`}>You did pretty good!</p>
-            <div className={styles.summaryDashGroup}>
-              <CircularDashboard
-                radius={60}
-                progressValue={240}
-                progressColor={'#2b788b'}
-                maxValue={320}
-                maxColor={'#c3dce3'}
-                strokeWidth={5}
-                startPoint={-90}
-                DashInfoComponent={() => <PointsDash progressValue={240} />}
-              />
-              <CircularDashboard
-                radius={60}
-                progressValue={30 + 10}
-                progressColor={'#639B6D'}
-                maxValue={40}
-                maxColor={'#c3dce3'}
-                strokeWidth={5}
-                startPoint={-90}
-                DashInfoComponent={() => <WordsDash progressValue={30} maxValue={40} />}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.summaryButtonGroup}>
-          <button>
-            <img src="./refresh.svg" alt="refresh" />
-            <p className="paragraph--p2">Play it again</p>
-          </button>
-          <Button color="light" label="Go to textbook" />
-        </div>
+    <SummaryFrame summaryHeader="Your Sprint">
+      <div className={styles.summaryDashGroup}>
+        <CircularDashboard
+          radius={60}
+          progressValue={240}
+          progressColor={'#2b788b'}
+          maxValue={320}
+          maxColor={'#c3dce3'}
+          strokeWidth={5}
+          startPoint={-90}
+          DashInfoComponent={() => <PointsDash progressValue={240} />}
+        />
+        <CircularDashboard
+          radius={60}
+          progressValue={30 + 10}
+          progressColor={'#639B6D'}
+          maxValue={40}
+          maxColor={'#c3dce3'}
+          strokeWidth={5}
+          startPoint={-90}
+          DashInfoComponent={() => <WordsDash progressValue={30} maxValue={40} />}
+        />
       </div>
-
-      <div className={styles.vocabularyListContainer}>
-        <div className="knowList">
-          <TitleList
-            titleName="I know"
-            labelName="15 words"
-            labelColor="#2B788B"
-            labelBgColor="#C3DCE3"
-          />
-        </div>
-        <div className="unKnowList">
-          <TitleList
-            titleName="I don't know"
-            labelName="15 words"
-            labelColor="#945069"
-            labelBgColor="#F2D4DC"
-          />
-        </div>
-      </div>
-    </section>
+    </SummaryFrame>
   );
 };
 
