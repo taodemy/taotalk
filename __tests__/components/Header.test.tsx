ba4bc9187617387dcd51eca29148f1976dcb973b
Jason Wang <0212jason@gmail.com>
2023-06-25 22:27:19 +1000

    fix: move header componeent out of the home folder it is unrelated to ticket

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Header from '../../src/components/Home/Header';
+import Header from '../../src/components/Header';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Header', () => {
