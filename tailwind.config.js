7434f95dc1a23cb75821a1e63d7f282db397bfb4
Jason Wang <0212jason@gmail.com>
2023-06-28 20:01:03 +1000

    feat: audiocall page without buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,32 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Primary Color*/
+        cyanDark: '#2b788b',
+        cyanLight: '#c3dce3',
+        pinkDark: '#945069',
+        pinkLight: '#f2d4dc',
+        black: '#000000',
+        greyLight: '#f6f5f4',
+        greyMedium: '#e0e0e0',
+        greyIcon: '#bababa',
+        greyDark: '#757575',
+        Zambezi: '#585858',
+
+        /* Secondary Color*/
+        cyan: '#5996a5',
+        green: '#639b6d',
+        pink: '#a15993',
+        red: '#a95151',
+        yellow: '#c4a24c',
+        orange: '#cb5b43',
+
+        /* Generally used color*/
+        white: '#ffffff',
+      },
+    },
   },
   plugins: [],
 };
