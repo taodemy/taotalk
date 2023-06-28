742cd4dbae40e6ec7e4a5b830e0f98a83ef1db46
Jason Wang <0212jason@gmail.com>
2023-06-28 20:07:35 +1000

    feat: responsive implementation of other section page draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '320px',
+      // => @media (min-width: 320px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
