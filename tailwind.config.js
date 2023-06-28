90b2ed68071218696c267b60330dbd7eadc737d5
Jason Wang <0212jason@gmail.com>
2023-06-28 22:22:37 +1000

    feat: add media query treshold for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '320px',
+        // => @media (min-width: 320px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 };
