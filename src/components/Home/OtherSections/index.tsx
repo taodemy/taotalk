fdda4c3ff9cbd7dcd3cce680f0ab5edfacc39d55
Jason Wang <0212jason@gmail.com>
2023-06-29 18:42:51 +1000

    feat: implement reponsive design of last two parts in the other sections of home page

--- src/components/Home/OtherSections/index.tsx
+++ src/components/Home/OtherSections/index.tsx
@@ -30,20 +30,24 @@ const OtherSections = () => {
         </div>
       </section>
       <section className="bg-gray-100">
-        <div className="mx-auto grid sm:px-[8px] sm:py-[16px] md:max-w-[1216px] md:px-[32px] md:py-[40px] xl:max-w-[1180px] xl:grid-cols-2 xl:gap-[26px] xl:pb-0 xl:pt-[26px]">
-          <div className="mx-0 my-auto flex flex-col sm:ml-0 xl:ml-[100px] xl:block">
+        <div className="mx-auto grid sm:px-[8px] sm:py-[16px] md:px-[32px] md:py-[40px] lg:max-w-[1180px] lg:grid-cols-2 lg:px-[0] lg:pb-[0] lg:pt-[26px] xl:gap-[26px] xl:px-[0] xl:pb-0">
+          <div className="mx-0 flex flex-col sm:ml-0 sm:py-[16px] md:py-[60px] lg:my-auto lg:ml-[100px] lg:block">
             <HeadingDescription
               headingContent="Increase your vocabulary"
               descriptionContent="Traditional and new effective approaches to word study"
             />
             <Button outline={false} color="cyanLight" label="Textbook →" />
           </div>
-          <img src="/vocabulary.svg" alt="Vocabulary" className="mx-0 my-auto w-full" />
+          <img src="/vocabulary.svg" alt="Vocabulary" className="mx-0 my-auto sm:w-full" />
         </div>
       </section>
-      <section className="mx-auto grid sm:px-[8px] sm:py-[16px] md:max-w-[1216px] md:px-[32px] md:py-0 xl:max-w-[1180px] xl:grid-cols-2 xl:gap-[20px] xl:py-[95px] ">
-        <img src="/progress.svg" alt="Statistics" className="mx-0 my-auto w-full" />
-        <div className="mx-0 my-auto flex flex-col xl:block">
+      <section className="mx-auto grid sm:px-[8px] sm:py-[16px] md:px-[32px] md:py-0 lg:max-w-[1180px] lg:grid-cols-2 lg:px-[0] lg:py-[95px] xl:gap-[20px] xl:px-[0] ">
+        <img
+          src="/progress.svg"
+          alt="Statistics"
+          className="mx-0 my-auto sm:mb-[60px] sm:w-full md:mb-[0] lg:h-[409px] lg:w-auto"
+        />
+        <div className="mx-0 flex flex-col sm:pb-[60px] md:pb-[120px] md:pt-[60px] lg:my-auto lg:ml-auto lg:block lg:py-[0] xl:max-w-[475px]">
           <HeadingDescription
             headingContent="Watch your progress every day"
             descriptionContent="Save statistics on your achievements, words learned, and mistakes"
