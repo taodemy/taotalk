54d2bdeae5858639b419c60199a70cc9544a57c3
Jason Wang <0212jason@gmail.com>
2023-06-29 11:49:07 +1000

    feat: implement audicall category

--- src/pages/audiocall-category.tsx
+++ src/pages/audiocall-category.tsx
@@ -5,9 +5,9 @@ import Button from '../components/Shared/Buttons';
 
 export default function AudioCallCategoryPage() {
   return (
-    <section className="bg-[#f6f5f4]">
+    <section className="bg-[#f6f5f4] py-[100px]">
       <div className="shadow-[0_23px_46px_-12px_rgba(52, 41, 39, 0.08)] mx-auto max-w-[737px] rounded-[14px] bg-white">
-        <div className="flex gap-[10px] py-[20px] pl-[50px]">
+        <div className="flex gap-[10px] py-[20px] pl-[50px] ">
           <AudioGame />
           <div className="my-auto pr-[51px]">
             <div className="flex gap-[10px] pb-[14px]">
@@ -23,9 +23,13 @@ export default function AudioCallCategoryPage() {
             </p>
             <div className="pb-[30px]">
               <p className="paragraph--p3 pb-[8px]">Choose a level:</p>
-              <div>
-                {/* <Button color="cyanDark" outline={true} label="C2" className="px-[13px]" /> */}
-                <Button color="cyanDark" outline={true} label="C2" />
+              <div className="flex gap-[24px]">
+                <Button color="green" outline={true} size="round" label="A1" />
+                <Button color="yellow" outline={true} size="round" label="A2" />
+                <Button color="orange" outline={true} size="round" label="B1" />
+                <Button color="red" outline={true} size="round" label="B2" />
+                <Button color="pink" outline={true} size="round" label="C1" />
+                <Button color="cyan" outline={true} size="round" label="C2" />
               </div>
             </div>
             <Button color="cyanDark" outline={false} label="Get started" />
