6469c96d7e3f8dbf061882c4d0e54ef9ececa02b
Jason Wang <0212jason@gmail.com>
2023-06-30 10:43:11 +1000

    fix: change only screen media query to overwrite and the other extend to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,110 +9,53 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontSize: {
-      sm: [
-        '0.75rem',
-        {
-          lineHeight: '140%',
-          fontWeight: '600',
-        },
-      ],
-      base: [
-        '0.875rem',
-        {
-          lineHeight: '140%',
-          fontWeight: '600',
-        },
-      ],
-      lg: [
-        '1rem',
-        {
-          lineHeight: '140%',
-          fontWeight: '600',
-        },
-      ],
-      xl: [
-        '1.125rem',
-        {
-          lineHeight: '140%',
-          fontWeight: '600',
-        },
-      ],
-      '2xl': [
-        '1.625rem',
-        {
-          lineHeight: '100%',
-          fontWeight: '400',
-        },
-      ],
-      '3xl': [
-        '2rem',
-        {
-          lineHeight: '100%',
-          fontWeight: '400',
-        },
-      ],
-      '4xl': [
-        '3rem',
-        {
-          lineHeight: '100%',
-          fontWeight: '400',
-        },
-      ],
-      '5xl': [
-        '3.5rem',
-        {
-          lineHeight: '120%',
-          fontWeight: '400',
-        },
-      ],
-    },
-    fontFamily: {
-      sans: ['Dela Gothic One', 'Montserrat'],
+    screens: {
+      sm: '320px',
+      // => @media (min-width: 320px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      xl: '1920px',
+      // => @media (min-width: 1920px) { ... }
     },
     extend: {
-      screens: {
-        sm: '320px',
-        // => @media (min-width: 320px) { ... }
-
-        md: '768px',
-        // => @media (min-width: 768px) { ... }
-
-        lg: '1280px',
-        // => @media (min-width: 1280px) { ... }
-
-        xl: '1920px',
-        // => @media (min-width: 1920px) { ... }
+      fontFamily: {
+        sans_Dela_Gothic_One: 'Dela Gothic One',
+        sans_Montserrat: 'Montserrat',
+      },
+      fontSize: {
+        tk12: '0.75rem', //12px
+        tk14: '0.875rem', //14px
+        tk16: '1rem', //16px
+        tk18: '1.125rem', //18px
+        tk26: '1.625rem', //26px
+        tk32: '2rem', //32px
+        tk48: '3rem', //48px
+        tk56: '3.5rem', //56px
       },
       colors: {
         /* Primary Color*/
-        cyanDark: '#2b788b',
-        cyanLight: '#c3dce3',
-        pinkDark: '#945069',
-        pinkLight: '#f2d4dc',
-        black: '#000000',
-        greyLight: '#f6f5f4',
-        greyMedium: '#e0e0e0',
-        greyIcon: '#bababa',
-        greyDark: '#757575',
-        Zambezi: '#585858',
+        tk_cyanDark: '#2b788b',
+        tk_cyanLight: '#c3dce3',
+        tk_pinkDark: '#945069',
+        tk_pinkLight: '#f2d4dc',
+        tk_greyLight: '#f6f5f4',
+        tk_greyMedium: '#e0e0e0',
+        tk_greyIcon: '#bababa',
+        tk_greyDark: '#757575',
+        tk_Zambezi: '#585858',
 
         /* Secondary Color*/
-        cyan: '#5996a5',
-        green: {
-          DEFAULT: '#639b6d',
-          LIGHTEN: 'rgba(99, 155, 109, 0.3)',
-        },
-        pink: '#a15993',
-        red: '#a95151',
-        yellow: '#c4a24c',
-        orange: {
-          DEFAULT: '#cb5b43',
-          LIGHTEN: 'rgba(203, 91, 67, 0.3)',
-        },
-
-        /* Generally used color*/
-        $white: '#ffffff',
+        tk_cyan: '#5996a5',
+        tk_green: '#639b6d',
+        tk_pink: '#a15993',
+        tk_red: '#a95151',
+        tk_yellow: '#c4a24c',
+        tk_orange: '#cb5b43',
       },
     },
   },
