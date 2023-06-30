d36099b0154f2d1e08796f298516c5a76bf28585
Jason Wang <0212jason@gmail.com>
2023-06-30 17:27:29 +1000

    fix: solve merge conflicts

--- src/components/Home/OtherSections/HeadingDescription/index.tsx
+++ src/components/Home/OtherSections/HeadingDescription/index.tsx
@@ -7,9 +7,13 @@ interface HeadingDescriptionProps {
 
 const HeadingDescription = ({ headingContent, descriptionContent }: HeadingDescriptionProps) => {
   return (
-    <div className="headingDescriptionContainer">
-      <h2 className="heading--h2">{headingContent}</h2>
-      <p className="paragraph--p1">{descriptionContent}</p>
+    <div className="xs:flex xs:flex-col">
+      <h2 className="heading--h2 xl:w-9/10 xs:w-full xs:text-center xs:text-[32px] md:text-[48px] xl:text-left">
+        {headingContent}
+      </h2>
+      <p className="paragraph--p1 xs:w-full xs:py-[24px] xs:text-center xs:text-[12px] md:text-[18px] xl:w-3/4 xl:text-left">
+        {descriptionContent}
+      </p>
     </div>
   );
 };
