ef02e6e1c3f2274d0ec8a1a2ed60bbc135e3f441
Jason Wang <0212jason@gmail.com>
2023-07-04 18:19:17 +1000

    fix: crop hero section image while in mobile and tablet mode and remove mobile media query symbol

--- src/components/Home/HeroSection/index.tsx
+++ src/components/Home/HeroSection/index.tsx
@@ -2,21 +2,21 @@ import Button from '../../Shared/Buttons';
 const Hero = () => {
   return (
     <article className="bg-tk_greyLight">
-      <section className="sm:px-2 sm:pt-4 md:px-[32px] md:py-0 lg:pl-[calc(370px-(1920px-100%)*0.5)] lg:pr-[calc(117px-(1920px-100%)*0.5)]">
+      <section className="px-2 pt-4 md:px-[32px] md:py-0 lg:pl-[calc(370px-(1920px-100%)*0.5)] lg:pr-[calc(117px-(1920px-100%)*0.5)]">
         <div className="grid overflow-hidden lg:grid-cols-[531px_minmax(900px,auto)]">
-          <div className="sm:mx-auto sm:flex sm:flex-col md:w-full lg:block lg:pt-[95px]">
-            <p className="p2 text-tk_cyanDark sm:pb-[24px] sm:pt-[40px] sm:text-center sm:text-[12px] md:pt-[97px] lg:pb-[24px] lg:pt-0 lg:text-left lg:font-bold lg:uppercase lg:leading-[1rem] lg:tracking-[0.14em]">
+          <div className="mx-auto flex flex-col md:w-full lg:block lg:pt-[95px]">
+            <p className="p2 pb-[24px] pt-[40px] text-center text-[12px] text-tk_cyanDark md:pt-[97px] lg:pb-[24px] lg:pt-0 lg:text-left lg:font-bold lg:uppercase lg:leading-[1rem] lg:tracking-[0.14em]">
               AI-powered chatting PLATFORM
             </p>
-            <h1 className="sm:mx-auto sm:text-center sm:text-[32px] md:max-w-[480px] md:text-[56px] lg:ml-0 lg:text-left">
+            <h1 className="mx-auto text-center text-[32px] md:max-w-[480px] md:text-[56px] lg:ml-0 lg:text-left">
               Learning and teaching online, made easy.
             </h1>
-            <p className="p1 sm:mx-auto sm:w-[304px] sm:pb-[32px] sm:pt-[30px] sm:text-center sm:text-[14px] md:w-[528px] md:text-[18px] lg:ml-0 lg:w-[397px] lg:text-left">
+            <p className="p1 mx-auto w-[304px] pb-[32px] pt-[30px] text-center text-[14px] md:w-[528px] md:text-[18px] lg:ml-0 lg:w-[397px] lg:text-left">
               Practice your English and learn new things with the platform.
             </p>
             <Button outline={false} color="cyanLight" label="About Platform" />
-            <div className="mx-auto flex sm:py-[40px] md:pb-[97px] lg:pt-[49px]">
-              <div className="sm:pr-[16px] md:pr-[49px]">
+            <div className="mx-auto flex py-[40px] md:pb-[97px] lg:pt-[49px]">
+              <div className="pr-[16px] md:pr-[49px]">
                 <div className="flex">
                   <img src="/heroLightening.svg" alt="heroLightening icon" className="pr-[10px]" />
                   <h3 className="text-[40px] tracking-[-0.02em]">
@@ -26,7 +26,7 @@ const Hero = () => {
                 <p className="p2 px-[3px] py-[3px] text-center text-tk_zambezi">Popular words</p>
               </div>
               <div className="h-[48.78px] w-[1.22px] bg-tk_greyMedium"></div>
-              <div className="sm:pl-[16px] md:pl-[49px]">
+              <div className="pl-[16px] md:pl-[49px]">
                 <div className="flex">
                   <img src="/heroGame.svg" alt="heroGame icon" className="pr-[13px]" />
                   <h3 className="text-[40px] tracking-[-0.02em]">
@@ -37,8 +37,10 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="sm:w-[130%] lg:w-[933px] lg:pb-[30px] lg:pt-[109px]">
-            <img src="/heroBoy.svg" alt="heroBoy image" className="sm:w-full lg:h-[601px]" />
+          <div className="h-[260px] overflow-hidden md:h-[671px] lg:h-[601px] lg:pb-[30px] lg:pt-[109px]">
+            <div className="w-[130%] lg:w-full">
+              <img src="/heroBoy.svg" alt="heroBoy image" className="w-full" />
+            </div>
           </div>
         </div>
       </section>
--- src/components/Home/OtherSections/index.tsx
+++ src/components/Home/OtherSections/index.tsx
@@ -6,21 +6,21 @@ import styles from './OtherSections.module.scss';
 const OtherSections = () => {
   return (
     <>
-      <section className="mx-auto grid sm:px-[8px] sm:py-[16px] lg:max-w-[1180px] lg:grid-cols-2 lg:gap-[120px] lg:py-[78px]">
+      <section className="mx-auto grid px-[8px] py-[16px] lg:max-w-[1180px] lg:grid-cols-2 lg:gap-[120px] lg:py-[78px]">
         <img
           src="/language.svg"
           alt="Language"
-          className="sm:hidden md:mx-auto md:block lg:my-auto lg:max-h-[445px] lg:max-w-[580px]"
+          className="hidden md:visible md:mx-auto md:block md:h-[500px] lg:my-auto lg:h-[445px]"
         />
-        <div className="flex flex-col sm:items-center sm:py-[60px] md:order-first md:py-[115px] lg:order-last lg:my-auto lg:items-start">
+        <div className="flex flex-col items-center py-[60px] md:order-first md:py-[115px] lg:order-last lg:my-auto lg:items-start">
           <HeadingDescription
             headingContent="Learn a language in a playful way"
             descriptionContent="Make learning words more fun with mini-games"
           />
-          <div className="flex sm:min-h-[150px] sm:gap-[20px] lg:gap-[21px]">
+          <div className="flex min-h-[150px] gap-[20px] lg:gap-[21px]">
             <div className="flex flex-col">
               <img
-                className="z-10 hover:translate-y-[-3px] sm:h-[112px] sm:w-[134px] lg:z-10 lg:h-[106px]"
+                className="z-10 h-[112px] w-[134px] hover:translate-y-[-3px] lg:z-10 lg:h-[106px]"
                 src="/sprint.svg"
                 alt="sprint"
               />
@@ -32,12 +32,12 @@ const OtherSections = () => {
             </div>
             <div className="flex flex-col items-center">
               <img
-                className="z-10 hover:translate-y-[-3px] sm:h-[112px] sm:w-[112px] lg:h-[106px]"
+                className="z-10 h-[112px] w-[112px] hover:translate-y-[-3px] lg:h-[106px]"
                 src="/audio-call.svg"
                 alt="video call"
               />
               <div>
-                <span className="p2 rounded-[14px] bg-tk_cyanLight font-bold text-tk_cyanDark sm:px-[18px] sm:pb-[14px] sm:pt-[82px] lg:px-[34px] lg:pb-[14px] lg:pt-[82px]">
+                <span className="p2 rounded-[14px] bg-tk_cyanLight px-[18px] pb-[14px] pt-[82px] font-bold text-tk_cyanDark lg:px-[34px] lg:pb-[14px] lg:pt-[82px]">
                   Audio-call →
                 </span>
               </div>
