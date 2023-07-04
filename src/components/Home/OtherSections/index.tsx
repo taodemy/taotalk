99feac4265e5e09b825eadef73633a22ea2eff6a
Jason Wang <0212jason@gmail.com>
2023-07-04 13:23:34 +1000

    fix: remove some unnecessary code like small screen media tag 2adding height to control image size

--- src/components/Home/OtherSections/index.tsx
+++ src/components/Home/OtherSections/index.tsx
@@ -6,7 +6,7 @@ import styles from './OtherSections.module.scss';
 const OtherSections = () => {
   return (
     <>
-      <section className={styles.languageSectionContainer}>
+      {/* <section className={styles.languageSectionContainer}>
         <img src="/language.svg" alt="Language" className={styles.imgContainerfull} />
         <div>
           <HeadingDescription
@@ -28,26 +28,31 @@ const OtherSections = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       <section className="bg-tk_greyLight">
-        <div className="mx-auto grid sm:px-2 sm:py-4 md:px-8 md:py-10 lg:max-w-[1180px] lg:grid-cols-2 lg:px-0 lg:pb-0 lg:pt-[26px] xl:gap-[26px] xl:px-0 xl:pb-0">
-          <div className="mx-0 flex flex-col sm:ml-0 sm:py-4 md:py-[60px] lg:my-auto lg:ml-[100px] lg:block">
+        <div className="mx-auto grid px-2 py-4 md:px-8 md:py-10 lg:max-w-[1180px] lg:grid-cols-2 lg:px-0 lg:pb-0 lg:pt-[26px]">
+          <div className="mx-0 ml-0 flex flex-col py-4 md:py-[60px] lg:my-auto lg:ml-[100px] lg:block">
             <HeadingDescription
               headingContent="Increase your vocabulary"
               descriptionContent="Traditional and new effective approaches to word study"
             />
             <Button outline={false} color="cyanLight" label="Textbook →" />
           </div>
-          <img src="/vocabulary.svg" alt="Vocabulary" className="mx-0 my-auto sm:w-full" />
+          <img
+            src="/vocabulary.svg"
+            alt="Vocabulary"
+            className="mx-auto my-auto h-[200px] md:h-[363px] lg:h-auto
+            "
+          />
         </div>
       </section>
-      <section className="mx-auto grid sm:px-2 sm:py-4 md:px-8 md:py-0 lg:max-w-[1180px] lg:grid-cols-2 lg:px-0 lg:py-[95px] xl:gap-[20px] xl:px-0 ">
+      <section className="mx-auto grid px-2 py-4 md:px-8 md:py-0 lg:max-w-[1180px] lg:grid-cols-2 lg:px-0 lg:py-[95px] xl:gap-[20px] xl:px-0 ">
         <img
           src="/progress.svg"
           alt="Statistics"
-          className="mx-0 my-auto sm:mb-[60px] sm:w-full md:mb-0 lg:h-[409px] lg:w-auto"
+          className="mx-auto my-auto mb-[60px] h-[200px] md:mb-0 md:h-[373px] lg:h-auto"
         />
-        <div className="mx-0 flex flex-col sm:pb-[60px] md:pb-[120px] md:pt-[60px] lg:my-auto lg:ml-auto lg:block lg:py-0 xl:max-w-[475px]">
+        <div className="mx-0 flex flex-col pb-[60px] md:pb-[120px] md:pt-[60px] lg:my-auto lg:ml-auto lg:block lg:py-0 xl:max-w-[475px]">
           <HeadingDescription
             headingContent="Watch your progress every day"
             descriptionContent="Save statistics on your achievements, words learned, and mistakes"
