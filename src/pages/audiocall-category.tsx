64c327b4d5e61a16cb4090e38d5564d86018d5f6
Jason Wang <0212jason@gmail.com>
2023-07-05 14:33:45 +1000

    fix: add extra hovering color for round buttons

--- src/pages/audiocall-category.tsx
+++ src/pages/audiocall-category.tsx
@@ -5,24 +5,26 @@ import Button from '../components/Shared/Buttons';
 
 export default function AudioCallCategoryPage() {
   return (
-    <section className="bg-[#f6f5f4] py-[100px]">
+    <section className="bg-tk_greyLight py-[100px]">
       <div className="shadow-[0_23px_46px_-12px_rgba(52, 41, 39, 0.08)] mx-auto max-w-[737px] rounded-[14px] bg-white">
-        <div className="flex gap-[10px] py-[20px] pl-[50px] ">
-          <AudioGame />
-          <div className="my-auto pr-[51px]">
+        <div className="flex gap-[10px] py-[20px] pl-[50px]">
+          <div className="hidden min-w-[250px] md:block">
+            <AudioGame />
+          </div>
+          <div className="mx-auto pr-[51px] sm:my-[63px] md:my-auto">
             <div className="flex gap-[10px] pb-[14px]">
-              <h2 className="heading--h2 whitespace-nowrap">Audio-Call</h2>
+              <h2 className="whitespace-nowrap">Audio-Call</h2>
               <div className="my-auto">
                 <Label fontColor="#2b788b" bgColor="#c3dce3">
                   audition task
                 </Label>
               </div>
             </div>
-            <p className="paragraph--p2 max-w-[383px] pb-[30px]">
+            <p className="p2 max-w-[383px] pb-[30px]">
               The Audio-Call training develops vocabulary and improves listening comprehension.
             </p>
             <div className="pb-[30px]">
-              <p className="paragraph--p3 pb-[8px]">Choose a level:</p>
+              <p className="p3 pb-2">Choose a level:</p>
               <div className="flex gap-[24px]">
                 <Button color="green" outline={true} size="round" label="A1" />
                 <Button color="yellow" outline={true} size="round" label="A2" />
