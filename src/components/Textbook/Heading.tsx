9ad634455f5b2899b58dbd0d0db8ea0ab713a9a4
LULIU-9 <lu.liu4work@gmail.com>
2023-07-08 12:05:38 +1000

    feat: refactor json object

--- src/components/Textbook/Heading.tsx
+++ src/components/Textbook/Heading.tsx
@@ -13,66 +13,66 @@ interface HeadingProps {
 const Heading = (props: HeadingProps) => {
   const router = useRouter();
   return (
-    <div>
-      <div className={Styles.heading}>
-        <div className={Styles.icon}>
-          <img src="hat.svg" alt="hat icon" />
-          <h4 className="heading--h4">Textbook</h4>
+    <div className={Styles.heading}>
+      <div className={Styles.icon}>
+        <img src="hat.svg" alt="hat icon" />
+        <h4 className="heading--h4">Textbook</h4>
+      </div>
+
+      <div className={Styles.buttons}>
+        <div className={Styles.sprint}>
+          <div>
+            <img src="shoe.svg" alt="sprint icon" />
+          </div>
+          <Button
+            outline={false}
+            color="pinkLight"
+            label="Sprint"
+            onClick={() => router.push('/sprint-summary')}
+          />
         </div>
-
-        <div className={Styles.buttons}>
-          <div className={Styles.sprint}>
-            <div>
-              <img src="shoe.svg" alt="sprint icon" />
-            </div>
-            <Button
-              outline={false}
-              color="pinkLight"
-              label="Sprint"
-              onClick={() => router.push('/sprint-summary')}
-            />
+        <div className={Styles.audio}>
+          <div>
+            <img src="speaker.svg" alt="sprint icon" />
           </div>
-          <div className={Styles.audio}>
-            <div>
-              <img src="speaker.svg" alt="sprint icon" />
-            </div>
-            <Button
-              outline={false}
-              color="cyanLight"
-              label="Audio-Call"
-              onClick={() => router.push('/audiocall-summary')}
-            />
+          <Button
+            outline={false}
+            color="cyanLight"
+            label="Audio-Call"
+            onClick={() => router.push('/audiocall-summary')}
+          />
+        </div>
+        <div
+          className={Styles.icon}
+          onMouseEnter={props.handleDropdown}
+          onMouseLeave={props.handleDropdown}
+        >
+          <div className={Styles.setting}>
+            <Setting alt="setting icon" />
           </div>
-          <div
-            className={Styles.icon}
-            onMouseEnter={props.handleDropdown}
-            onMouseLeave={props.handleDropdown}
-          >
-            <div className={Styles.setting}>
-              <Setting alt="setting icon" />
-            </div>
 
-            {props.isDropdownOpen && (
-              <div className={Styles.dropdownContainer}>
-                <ul>
-                  <li>
-                    <label className={`paragraph--p2 ${Styles.container}`} htmlFor="viewCheck">
-                      Show words in a list
-                      <input type="checkbox" id="viewCheck" name="viewCheck" value="true" />
-                      <span className={Styles.checkmark}></span>
-                    </label>
-                  </li>
-                  <li>
-                    <label className={`paragraph--p2 ${Styles.container}`} htmlFor="buttonCheck">
+          {props.isDropdownOpen && (
+            <div className={Styles.dropdownContainer}>
+              <ul>
+                <li>
+                  <label className={Styles.container} htmlFor="viewCheck">
+                    <p className="paragraph--p2 ">Show words in a list</p>
+                    <input type="checkbox" id="viewCheck" name="viewCheck" value="true" />
+                    <span className={Styles.checkmark}></span>
+                  </label>
+                </li>
+                <li>
+                  <label className={Styles.container} htmlFor="buttonCheck">
+                    <p className="paragraph--p2">
                       Display the &apos;Add&apos; and &apos;Remove&apos; buttons
-                      <input type="checkbox" id="buttonCheck" name="buttonCheck" value="true" />
-                      <span className={Styles.checkmark}></span>
-                    </label>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
+                    </p>
+                    <input type="checkbox" id="buttonCheck" name="buttonCheck" value="true" />
+                    <span className={Styles.checkmark}></span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
