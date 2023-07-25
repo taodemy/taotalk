e5a5b3fb662c4d84bd269b20a7e6e446f6bacaad
Jason Wang <0212jason@gmail.com>
2023-07-25 16:27:01 +1000

    feat: implement sign in and signout using next auth

--- src/layout/Header/LogInButton.tsx
+++ src/layout/Header/LogInButton.tsx
@@ -1,13 +1,21 @@
 import styles from './Header.module.scss';
 
-export default function LogInButton({ userName }: { userName: string }) {
+export default function LogInButton({
+  userName,
+  onClick,
+}: {
+  userName: string;
+  onClick: () => void;
+}) {
   return (
     <div className={styles.account_label}>
       <div className={styles.account_label__user_name_div}>
         <div className={styles.account_label__first_letter}>{getFirstCapLetter(userName)}</div>
         <div className={styles.account_label__user_name}> {userName}</div>
       </div>
-      <div className={styles.account_label__sign_out}>Sign Out →</div>
+      <div className={styles.account_label__sign_out} onClick={onClick}>
+        Sign Out →
+      </div>
     </div>
   );
 }
